8c0731c69866a58ad71838ed9d5fd7b3e8010892
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2025-07-18 14:06:55 +0200

    init

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,17 @@
-import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/app.scss';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
